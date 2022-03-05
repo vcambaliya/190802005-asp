6cd6bba5dea85df1cc99155872e95a8f95c12b8b
Vidhi Ambaliya <vidhiambaliya20@gmail.com>
2022-03-06 00:34:38 +0530

    Third commit

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -13,17 +13,13 @@ public partial class Login : System.Web.UI.Page
     SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DatabaseConnectionString1"].ConnectionString);
     protected void Page_Load(object sender, EventArgs e)
     {
-        //DeleteCommand="DELETE FROM [users] WHERE [id] = @id" 
-        //InsertCommand="INSERT INTO [users] ([fullname], [email], [password]) VALUES (@fullname, @email, @password)" 
-        //ProviderName="<%$ ConnectionStrings:DatabaseConnectionString1.ProviderName %>" 
-        //SelectCommand="SELECT [id], [fullname], [email], [password] FROM [users]" 
-        //UpdateCommand="UPDATE [users] SET [fullname] = @fullname, [email] = @email, [password] = @password WHERE [id] = @id">
+        
     }
     protected void Button1_Click(object sender, EventArgs e)
     {
         try
         {
-            SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM [users] WHERE [email] = @email AND [password] = @password", con);
+            SqlCommand cmd = new SqlCommand("SELECT COUNT(*) FROM [userdata] WHERE [email] = @email AND [password] = @password", con);
             cmd.Parameters.AddWithValue("@email", TextBox1.Text.Trim());
             cmd.Parameters.AddWithValue("@password", TextBox2.Text.Trim());
             con.Open();
--- Registration.aspx.cs
+++ Registration.aspx.cs
@@ -13,15 +13,11 @@ public partial class Registration : System.Web.UI.Page
     SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DatabaseConnectionString1"].ConnectionString);
     protected void Page_Load(object sender, EventArgs e)
     {
-        //DeleteCommand="DELETE FROM [users] WHERE [id] = @id" 
-        //InsertCommand="INSERT INTO [users] ([fullname], [email], [password]) VALUES (@fullname, @email, @password)" 
-        //ProviderName="<%$ ConnectionStrings:DatabaseConnectionString1.ProviderName %>" 
-        //SelectCommand="SELECT [id], [fullname], [email], [password] FROM [users]" 
-        //UpdateCommand="UPDATE [users] SET [fullname] = @fullname, [email] = @email, [password] = @password WHERE [id] = @id">
+        
     }
     protected void Button1_Click(object sender, EventArgs e)
     {
-        SqlCommand cmd = new SqlCommand("INSERT INTO [users] ([fullname], [email], [password]) VALUES (@fullname, @email, @password)", con);
+        SqlCommand cmd = new SqlCommand("INSERT INTO [userdata] ([fullname], [email], [password]) VALUES (@fullname, @email, @password)", con);
         cmd.Parameters.AddWithValue("@fullname", TextBox1.Text.Trim());
         cmd.Parameters.AddWithValue("@email", TextBox2.Text.Trim());
         cmd.Parameters.AddWithValue("@password", TextBox3.Text.Trim());
